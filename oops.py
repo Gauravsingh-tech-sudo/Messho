927ff6167675f95482d79fd720a46a932d183d15
Gauravsingh-tech-sudo <Ajaykamleshsingh785@gmail.com>
2024-11-21 18:18:59 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,17 @@
+# class Student:
+#     name="gaurav"
+
+# s1=Student()
+# print(s1.name)
+# s2=Student()
+# print(s2.name)
+class Car:
+    color="Black"
+    brand="Ferrari"
+    speed="300"
+c1=Car()
+print(c1.color)
+c2=Car()
+print(c2.brand)
+c3=Car()
+print(c3.speed)
